0acdc18c37190e2d6a99ed3d8892abf2ce7c7167
Tim Hildebrandt <44113468+tim-hilde@users.noreply.github.com>
2024-05-30 10:52:35 +0200

    added viz and data load function

--- kestrix/visualization.py
+++ kestrix/visualization.py
@@ -1,28 +1,33 @@
 from keras_cv import visualization
 from kestrix.params import *
+from kestrix.data import load_dataset, prepare_dataset
 import tensorflow as tf
 
-def visualize_single_bounding_box(image:tf.Tensor, bbox:dict) -> None:
-    """Visualize a single bounding box.
+
+def visualize_bounding_box(path:str) -> None:
+    """Visualize one batch of bounding boxes.
 
     Args:
-        image (tf.Tensor): Image as a tensor.
-        bbox (dict): A dictionary of the format
-                    ```
-                    {
-                        "classes": [classes],
-                        "bounding_boxes": [lists of the coordinates of the boxes]
-                    }
-                    ```
+        path (str): Path to the images/txt
     """
+
+    prepared_dataset = prepare_dataset(path)
+
+    loaded_dataset = prepared_dataset.map(load_dataset, num_parallel_calls=tf.data.AUTOTUNE)
+
+    batched_dataset = loaded_dataset.ragged_batch(BATCH_SIZE, drop_remainder=True)
+
+    inputs = next(iter(batched_dataset.take(1)))
+
+    images, bounding_boxes = inputs["images"], inputs["bounding_boxes"]
     visualization.plot_bounding_box_gallery(
-            image,
-            value_range=(0, 255),
-            rows=1,
-            cols=1,
-            y_pred=bbox,
-            scale=5,
-            font_scale=0.7,
-            bounding_box_format=BOUNDING_BOX_FORMAT,
-            class_mapping=CLASS_MAPPING,
-        )
+        images,
+        value_range=(0, 255),
+        rows=2,
+        cols=2,
+        y_true=bounding_boxes,
+        scale=5,
+        font_scale=0.7,
+        bounding_box_format=BOUNDING_BOX_FORMAT,
+        class_mapping=CLASS_MAPPING,
+    )
