b3c161a57e5775e1a679bcfc186ecc2189d9117d
Francois-Xavier Foray <fx.foray@gmail.com>
2024-05-31 17:47:00 +0200

    FastAPI file added (to be updated for path).

--- kestrix/fast_api.py
+++ kestrix/fast_api.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI, File, UploadFile, Response
 import os
 from PIL import Image
 from io import BytesIO
+from tensorflow import keras
 
-from kestrix.model import load_model, predict
-from kestrix.postprocess import converting_coordinates_to_full_image, blur_bounding_boxes
+from kestrix.model import load_model, predict, compile_model
+from kestrix.postprocess import convert_coordinates_to_full_image, blur_bounding_boxes
 
 app = FastAPI()
+
+loaded_model =  keras.saving.load_model("/Users/foxidy/code/Max-c3/Kestrix_Project/models/compartment_20240531.keras")
+model = compile_model(loaded_model)
 
 @app.get("/")
 def root():
@@ -28,21 +32,31 @@ async def upload(file: UploadFile = File(...)):
 
         # Expand the user's home directory for saving the image
         ## --> Input correct input_image location
-        input_image_path = os.path.expanduser(f"~/Downloads/processed_{file.filename}")
+        input_folder = '/Users/foxidy/code/Max-c3/Kestrix_Project/data/input'
+        input_image_path = os.path.join(input_folder, f"{file.filename}")
+
         input_image.save(input_image_path, format='JPEG')
+
+        output_folder = '/Users/foxidy/code/Max-c3/Kestrix_Project/data/output'
+        output_file_name = os.path.splitext(os.path.basename(file.filename))[0]
+        output_image_path = os.path.join(output_folder, f"{output_file_name}_blurred.jpg")
+        print(output_image_path)
 
         ## Pipeline to apply Model predict, stitch batch bounding boxes coordinates and blur image
         # Apply Model predict
-        model = load_model("..models/")
         compartments_bounding_boxes = predict(input_image_path, model)
 
         # Stitch back bounding boxes of the compartments to image
-        image_bounding_boxes = converting_coordinates_to_full_image(compartments_bounding_boxes)
+        image_bounding_boxes = convert_coordinates_to_full_image(compartments_bounding_boxes)
 
         # Blur input image based of image bounding boxes
-        blur_bounding_boxes(image_bounding_boxes)
-        blurred_image_path = os.path.join(output_folder, f"{image_name}_blurred.jpeg")
-        blurred_image =
+        blur_bounding_boxes(input_image_path, image_bounding_boxes)
+
+        # Retrieve blurred image
+        output_folder = '/Users/foxidy/code/Max-c3/Kestrix_Project/data/output'
+        output_file_name = os.path.splitext(os.path.basename(file.filename))[0]
+        output_image_path = os.path.join(output_folder, f"{output_file_name}_blurred.jpg")
+        blurred_image = Image.open(output_image_path)
 
         # Save the image to a BytesIO object
         bytes_io = BytesIO()
